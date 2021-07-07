68989f2cc9456fb337be859c5d0d8dd032627b05
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-06 21:23:48 -0400

    create instances for the book class

--- app.js
+++ app.js
@@ -56,3 +56,11 @@ class Movie extends Media {
       return this._runTime;
     }
 }
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
