957d01f8ebc2d85af38d0cbb5cce4c81edc8b6e8
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-06 21:20:44 -0400

    add book class that extends the media class

--- app.js
+++ app.js
@@ -27,3 +27,17 @@ class Media {
       this.ratings.push(value);
     }
 }
+
+class Book extends Media {    
+    constructor(author, title, pages) {
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author() {
+      return this._author;
+    }  
+    get pages() {
+      return this._pages;
+    }
+}
