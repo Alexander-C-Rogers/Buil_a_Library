16e201949c05924e82c96859f993daba11c0d02f
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-08 11:51:53 -0400

    adding a Catalog for everthing and fixing the instances to show that

--- app.js
+++ app.js
@@ -79,7 +79,90 @@ class Cd extends Media {
   }
 }
 
-const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+const catalog = {
+  _books: [],
+  _movies: [],
+  _CDs: [],
+  get books() {
+    return this._books;
+  },
+  get movies() {
+    return this._movies;
+  },
+  get CDs() {
+    return this._CDs;
+  },
+  addBookToCatalog (title,author,pages) {
+    book = new Book(title,author,pages);
+    this._books.push(book);
+    return book;
+  },
+  removeBookFromCatalog (title,author) {
+    const books = this.books;
+    let bookToRemove = books.findIndex(function(book) {
+       return book.title === title && book.author === author;
+    });
+    this._books.splice(bookToRemove);
+ },
+  getBookFromCatalog (title,author) {
+    const books = this.books;
+    let findBook = books.find(function(book) {
+      return book.title === title && book.author === author;
+    })
+    return findBook;
+  },
+  addMovieToCatalog (title,director,runTime) {
+    const movie = new Movie(title,director,runTime);
+    this._movies.push(movie);
+  },
+  removeMovieFromCatalog (title,director) {
+    const movies = this.movies;
+    let movieToRemove = movies.findIndex(function(movie) {
+       return movie.title === title && movie.director === director;
+    });
+    this._movies.splice(movieToRemove);
+ },
+  getMovieFromCatalog (title,director) {
+    const movies = this.movies;
+    let findMovie = movies.find(function(movie) {
+      return movie.title === title && movie.director === director;
+    })
+    return findMovie;
+  },
+  addCDToCatalog (title,artist,songs) {
+    const cd = new CD(title,artist,songs);
+    this._CDs.push(cd);
+  },
+  removeCDFromCatalog (title,artist) {
+    const cds = this.cds;
+    let bookToRemove = cds.findIndex(function(cd) {
+       return cd.title === title && cd.artist === artist;
+    });
+    this._books.splice(bookToRemove);
+ },
+  getCDFromCatalog (title,artist) {
+    const cds = this.cds;
+    let findCD = cds.find(function(cd) {
+      return cd.title === title && cd.artist  === artist;
+    })
+    return findCD;
+  },
+  listCatalog () {
+    books = this.books;
+    movies = this.movies;
+    cds = this.cds;
+    if (books) {books.forEach(function(book) {console.log(`Book Title: ${book.title}, Author: ${book.author}, 
+    Checked out: ${book.toggleCheckOutStatus()}, Average rating: ${book.getAverageRating()}`)})};
+    if (movies) {movies.forEach(function(movie) {console.log(`Movie Title: ${movie.title}, Director: ${movie.director}, 
+    Checked out: ${movie.toggleCheckOutStatus()}, Average rating: ${movie.getAverageRating()}`)})};
+    if (cds) {cds.forEach(function(cd) {console.log(`Book Title: ${cd.title}, Artist: ${cd.artist}, 
+    Checked out: ${cd.toggleCheckOutStatus()}, Average rating: ${cd.getAverageRating()}`)})};
+  }
+}
+
+catalog.addBookToCatalog('A Short History of Nearly Everything','Bill Bryson',544);
+historyOfEverything = catalog.getBookFromCatalog('A Short History of Nearly Everything','Bill Bryson');
+console.log(historyOfEverything.isCheckedOut);
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut);
 historyOfEverything.addRating(4);
@@ -87,10 +170,13 @@ historyOfEverything.addRating(5);
 historyOfEverything.addRating(5);
 console.log(historyOfEverything.getAverageRating());
 
-const speed = new Movie('Jan de Bont', 'Speed', 116);
+catalog.addMovieToCatalog('Speed','Jan de Bont',116);
+speed = catalog.getMovieFromCatalog('Speed','Jan de Bont');
 speed.toggleCheckOutStatus();
 console.log(speed.isCheckedOut);
 speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
 console.log(speed.getAverageRating());
+
+catalog.listCatalog();
